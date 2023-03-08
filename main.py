2f39079227a5c2cd6c04924457c4b5dedd4c1a6f
roccovp3 <rocco.perciavalleiii@gmail.com>
2023-03-08 15:53:42 -0600

    can parse comments out

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     input_str = get_input()
-
+    print(create_command_array(input_str))
     return 0
 
 
@@ -14,12 +14,25 @@ def get_input():
     for arg in sys.argv[1:]:
         try:
             file = open(arg, 'r')
-            input_str += file.read()
+            for line in file.readlines():
+                if "//" in line and ";" in line:
+                    if line[::-1].index("//") < line[::-1].index(";"): #if comment is before
+                        line = line[0:line.index(";")+1]+'\n'
+                elif "//" in line and ";" not in line:
+                    continue
+                input_str += line
+            #input_str += file.read()
         except FileNotFoundError:
             print("Invaild Input")
     print("Input String:", input_str)
-    input_str.replace('\n', '')
+    input_str = input_str.replace('\n', '')
     return input_str
+
+
+def create_command_array(input_str):
+    command_array = input_str.split(";")
+    command_array = [x.strip() for x in command_array if (x != '')]
+    return command_array
 
 
 # Press the green button in the gutter to run the script.
--- test.qasm
+++ test.qasm
@@ -1,1 +1,7 @@
-test file
+//this is a comment
+this a;
+command g;
+//comment
+command g; //com f
+tes//t h; //bad line
+test efgh;

ebcffdc433a8d576179b24ffb9d8203140896595
roccovp3 <rocco.perciavalleiii@gmail.com>
2023-03-28 15:01:59 -0500

    css now works.

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
 import compute
+import os
+
+TEMPLATE_DIR = os.path.abspath('templates')
+STATIC_DIR = os.path.abspath('static')
+
+# app = Flask(__name__) # to make the app run without any
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
 app = Flask(__name__)
 @app.route("/")
-def index(): 
-    my_string = str(compute.compute())
-    return render_template("index.html", body=my_string)
+def index():
+    output = compute.compute()
+    print(STATIC_DIR)
+    return render_template("index.html", body=output)
+
+def parseCompute():
+    return
+
 
 if __name__ == "__main__":
     app.run()

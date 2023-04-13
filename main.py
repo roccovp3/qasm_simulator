e817e98bdc9ef729ee308fd1afba56c748c831b5
roccovp3 <rocco.perciavalleiii@gmail.com>
2023-04-13 15:50:52 -0500

    code now stays upon submission

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ STATIC_DIR = os.path.abspath('static')
 
 #app = Flask(__name__) # to make the app run without any
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
+
 @app.route("/", methods=('GET', 'POST'))
 def index():
     output = ''
     code = ''
+    if request.method == 'POST':
+        code = request.form['code']
     if request.method == 'POST' and request.form['button'] == "Calculate":
         output = compute.compute(int(request.form['shots']), request.form['code'])
     elif request.method == 'POST' and request.form['button'] == "Export":
@@ -25,8 +28,6 @@ def index():
         file.save(file.filename)
         file = open(file.filename)
         code = file.read()
-    #if code == '': bad request from this...
-        #code = request.form['code']
     print(STATIC_DIR)
     parseCompute(output)
     return render_template("index.html", body=output, code=code)
